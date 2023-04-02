25625598c101e3a9f887f99b93c8b23b9ce3ec3b
dheekkshaR <116768851+dheekkshaR@users.noreply.github.com>
2023-04-02 15:34:14 -0400

    a8 commit

---
+++ controllers/tuits/tuits.js
@@ -0,0 +1,96 @@
+export default [
+    /*{
+        "_id": "123", "topic": "Space", "username": "SpaceX",
+        "handle": "@spacex", "time": "2h", "image": "spacex.png",
+        "title": "SpaceX's Mission",
+        "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
+        "liked": true, "likes": 2345, "replies": 123, "retuits": 432
+    },
+    {
+        "_id": "234", "topic": "Traffic", "username": "The Boring Company",
+        "handle": "@boringcompany", "time": "2h", "image": "boring2.jpg",
+        "title": "The Boring Company fixing traffic",
+        "tuit": "The Boring Company (TBC) is an American infrastructure and tunnel construction services company founded by Elon Musk. Its ongoing and proposed projects are designed for intra-city ('loop') transit systems.",
+        "liked": true, "likes": 2345, "replies": 321, "retuits": 432
+    },
+    {
+        "_id": "345", "topic": "Brain Injuries", "username": "Neurolink",
+        "handle": "@neurolink", "time": "3h", "image": "neurolink.jpg",
+        "title": "Neurolink fixes paralisis and blindness",
+        "tuit": "Neurolink is an American company founded by Elon Musk that specializes in brain to computer integration. The first application will be to cure spinal cord injuries so that paraplegics can walk again. Other applications include curing blindness and mental disorders.",
+        "liked": true, "likes": 3456, "replies": 345, "retuits": 654
+    },
+    [*/
+        {
+            "idd": 123,
+            "avatar": "../../images/space.jpeg",
+            "userName": "Elon Musk",
+            "handle": "elonmusk",
+            "time": "23h",
+            "title": "Amazing how about @inspiration4x mission!",
+            "image": "../../images/elonpost.webp",
+            "imageTitle": "Countdown: Inspiration4 Mission to Space | Netflix Official Site",
+            "imageDescription": "From training to launch to landing, this all-access docuseries rides along with the Inspiration4 crew on the first all-civilian orbital space",
+            "imageLink": "netflix.com",
+            "comments": "420",
+            "retuits": "300",
+            "likes": "35",
+            "liked": true,
+            "dislikes":"10",
+            "disliked": false
+        },
+        {
+            "idd": 124,
+            "avatar": "../../images/nyp.png",
+            "userName": "New York Post",
+            "handle": "nypost",
+            "time": "23h",
+            "title": "Grimes seen reading Karl Marx following split with world's richest man Elon Musk",
+            "image": "../../images/tweee.png",
+            "imageTitle": "",
+            "imageDescription": "According to the timestamps on her original Myspace page, Boucher began writing music under the name Grimes in 2007. Her performer name was chosen because at the time, MySpace allowed artists to list three musical genres. She listed \"grime\" for all three, without knowing what the grime music genre was.",
+            "imageLink": "",
+            "comments": "965",
+            "retuits": "200",
+            "likes": "500",
+            "liked" : false,
+            "dislikes":"500",
+            "disliked": false
+        },
+        {
+            "idd": 129,
+            "avatar": "../../images/nyp.png",
+            "userName": "raju",
+            "handle": "ra",
+            "time": "23h",
+            "title": "Using the PostSummaryItem and PostSummaryList components as an example, create new TuitItem and TuitsList components that render the same list of tuits, but with a slightly different layout as shown here on the right. Create the new components under src/tuiter/tuits and feel free to reuse the HTML and CSS you created for the home screen in earlier assignments. Implement the tuit statistics such as the replies, retuits, and likes in a separate component called TuitStats that renders a row of four columns as shown below each tuit here on the right. The first column displays the number of replies next to a message bubble icon. The second column displays the number of retuits next to a retuits icon. ",
+            "image": "",
+            "imageTitle": "",
+            "imageDescription": "",  "imageLink": "",
+            "comments": "9",
+            "retuits": "2",
+            "likes": "4000",
+            "liked" : true,
+            "dislikes":"1000",
+            "disliked": false
+        },
+        {
+            "idd": 128,
+            "avatar": "../../images/nyp.png",
+            "userName": "Dheekksha",
+            "handle": "dhee",
+            "time": "23h",
+            "title": "The third column displays the number of likes next to a heart icon. If the tuit has been liked, the heart should render in red or empty otherwise. If users like a tuit, the likes count increases by one and the heart turns red. If they unlike the tuit, the likes count decreases by one and the heart is not red. The last column displays a share icon. Feel free to use icons that you think better represent the data. ",
+            "image": "",
+            "imageTitle": "",
+            "imageDescription": "",    "imageLink": "",
+            "comments": "95",
+            "retuits": "20",
+            "likes": "1000",
+            "liked" : false,
+            "dislikes":"100",
+            "disliked": true
+        }
+
+];
+
